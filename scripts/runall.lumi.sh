2abb99015cf78be0f4c42ece0b61a71bc7c65ec9
Linus Eickhoff <linusmaxeickhoff@gmail.com>
2025-05-16 11:12:38 +0300

    added lumi scripts and config

---
+++ scripts/runall.lumi.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# --------------------------------------------------------------------------------------
+# Part of the interTwin Project: https://www.intertwin.eu/
+#
+# Created by: Matteo Bunino
+#
+# Credit:
+# - Matteo Bunino <matteo.bunino@cern.ch> - CERN
+# - Linus Eickhoff <linus.maximilian.eickhoff@cern.ch> - CERN
+# --------------------------------------------------------------------------------------
+
+export CONTAINER_PATH="/project/project_465001592/hython-itwinai-plugin-containers/hython-itwinai-amd-2.sif"
+
+# Clear SLURM logs (*.out and *.err files)
+rm -rf logs_slurm checkpoints* mllogs* ray_checkpoints logs_torchrun
+mkdir -p logs_slurm logs_torchrun
+
+export HYDRA_FULL_ERROR=1
+
+# # DDP itwinai
+# DIST_MODE="ddp"
+# RUN_NAME="ddp-itwinai"
+# TRAINING_CMD="itwinai exec-pipeline --config-path configuration_files --config-name lumi_training strategy=ddp"
+# sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+#     --job-name="$RUN_NAME-n$N" \
+#     --output="logs_slurm/job-$RUN_NAME-n$N.out" \
+#     --error="logs_slurm/job-$RUN_NAME-n$N.err" \
+#     /users/eickhoff/hython-itwinai-plugin/scripts/slurm.lumi.sh
+
+# DeepSpeed itwinai
+DIST_MODE="deepspeed"
+RUN_NAME="deepspeed-itwinai"
+TRAINING_CMD="itwinai exec-pipeline --config-path configuration_files --config-name lumi_training strategy=deepspeed"
+sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$RUN_NAME-n$N" \
+    --output="logs_slurm/job-$RUN_NAME-n$N.out" \
+    --error="logs_slurm/job-$RUN_NAME-n$N.err" \
+    /users/eickhoff/hython-itwinai-plugin/scripts/slurm.lumi.sh
+
+# Horovod itwinai
+DIST_MODE="horovod"
+RUN_NAME="horovod-itwinai"
+TRAINING_CMD="itwinai exec-pipeline --config-path configuration_files --config-name lumi_training strategy=horovod"
+sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$RUN_NAME-n$N" \
+    --output="logs_slurm/job-$RUN_NAME-n$N.out" \
+    --error="logs_slurm/job-$RUN_NAME-n$N.err" \
+    /users/eickhoff/hython-itwinai-plugin/scripts/slurm.lumi.sh
+
