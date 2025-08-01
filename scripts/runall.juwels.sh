73ac0b213719f525b8da2f0c78aad1f662944024
Anjali Khantaal <Anjali.Khantaal@mbzuai.ac.ae>
2025-08-01 13:47:29 +0200

    updated runall with updated params

--- scripts/runall.juwels.sh
+++ scripts/runall.juwels.sh
@@ -16,7 +16,7 @@ PYTHON_VENV=".venv"
 # Clear SLURM logs (*.out and *.err files)
 read -p "Delete all existing scalability metrics and logs y/n?: " answer
 if [[ "$answer" =~ ^[Yy]$ ]]; then
-    rm -rf scalability-metrics logs_* checkpoints_* plots mllogs outputs ray_checkpoints
+    rm -rf scalability-metrics logs_* checkpoints* plots mllogs outputs ray_checkpoints
 fi
 mkdir -p logs_slurm
 
@@ -25,11 +25,12 @@ export HYDRA_FULL_ERROR=1
 # DDP itwinai
 DIST_MODE="ddp"
 RUN_NAME="hython-juwels-runall-ddp"
-NNODES=2
+NNODES=8
 NGPUS_PER_NODE=4
 TOT_GPUS=$(($NNODES * $NGPUS_PER_NODE))
+NUM_TRIALS=1
 
-TRAINING_CMD="itwinai exec-pipeline --config-path configuration_files --config-name juwels_training strategy=ddp run_name=hython-juwels-runall-ddp num_workers_per_trial=$TOT_GPUS"
+TRAINING_CMD="itwinai exec-pipeline --config-path configuration_files --config-name juwels_training strategy=ddp run_name=hython-juwels-runall-ddp num_workers_per_trial=$TOT_GPUS trials=$NUM_TRIALS"
 sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
     --job-name="$RUN_NAME-n$N" \
     --output="logs_slurm/job-$RUN_NAME-n$N.out" \
@@ -37,23 +38,24 @@ sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$T
     --nodes=$NNODES --gpus-per-node=$NGPUS_PER_NODE \
     ./scripts/slurm.juwels.sh
 
-# # DeepSpeed itwinai
-# DIST_MODE="deepspeed"
-# RUN_NAME="hython-juwels-runall-deepspeed"
-# TRAINING_CMD="itwinai exec-pipeline --config-path configuration_files --config-name juwels_training strategy=deepspeed run_name=hython-juwels-runall-deepspeed"
-# sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-#     --job-name="$RUN_NAME-n$N" \
-#     --output="logs_slurm/job-$RUN_NAME-n$N.out" \
-#     --error="logs_slurm/job-$RUN_NAME-n$N.err" \
-#     ./scripts/slurm.juwels.sh
-#
-# # Horovod itwinai
-# DIST_MODE="horovod"
-# RUN_NAME="hython-juwels-runall-horovod"
-# TRAINING_CMD="itwinai exec-pipeline --config-path configuration_files --config-name juwels_training strategy=horovod run_name=hython-juwels-runall-horovod"
-# sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-#     --job-name="$RUN_NAME-n$N" \
-#     --output="logs_slurm/job-$RUN_NAME-n$N.out" \
-#     --error="logs_slurm/job-$RUN_NAME-n$N.err" \
-#     ./scripts/slurm.juwels.sh
-#
+# DeepSpeed itwinai
+DIST_MODE="deepspeed"
+RUN_NAME="hython-juwels-runall-deepspeed"
+TRAINING_CMD="itwinai exec-pipeline --config-path configuration_files --config-name juwels_training strategy=deepspeed run_name=hython-juwels-runall-deepspeed num_workers_per_trial=$TOT_GPUS trials=$NUM_TRIALS"
+sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$RUN_NAME-n$N" \
+    --output="logs_slurm/job-$RUN_NAME-n$N.out" \
+    --error="logs_slurm/job-$RUN_NAME-n$N.err" \
+    --nodes=$NNODES --gpus-per-node=$NGPUS_PER_NODE \
+    ./scripts/slurm.juwels.sh
+
+# Horovod itwinai
+DIST_MODE="horovod"
+RUN_NAME="hython-juwels-runall-horovod"
+TRAINING_CMD="itwinai exec-pipeline --config-path configuration_files --config-name juwels_training strategy=horovod run_name=hython-juwels-runall-horovod num_workers_per_trial=$TOT_GPUS trials=$NUM_TRIALS"
+sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$RUN_NAME-n$N" \
+    --output="logs_slurm/job-$RUN_NAME-n$N.out" \
+    --error="logs_slurm/job-$RUN_NAME-n$N.err" \
+    --nodes=$NNODES --gpus-per-node=$NGPUS_PER_NODE \
+    ./scripts/slurm.juwels.sh
